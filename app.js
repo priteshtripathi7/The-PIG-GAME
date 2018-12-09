ad31f0dd2ba57b7339498d15df0b55ed018916e1
Pritesh Kumar Tripathi <pritesh.kt@gmail.com>
2018-12-09 21:54:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function gamePlay() {
     roundScore = 0;
     activePlayer = 0;
     isPlaying = true;
-    
+    document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
